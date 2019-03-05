d15d68f72e1b5fe421d72c14943545175ddb6ce4
wangding <408542507@qq.com>
2019-03-05 19:44:54 +0800

    1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,19 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
   grunt.initConfig({
-  less: {
-    development: {
-      files: {
-          'css/layout.css': 'less/layout.less'            
-      }    
+    csslint: {
+      options: {
+                csslintrc: '.csslintrc'
+          
+      },
+        src: ['rectangle.css']
+        
     }
-  }
-});
-grunt.loadNpmTasks('grunt-contrib-less');
-grunt.registerTask('default',['less']);
+               
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+
+      grunt.registerTask('default', ['csslint']);
+
 };
+
